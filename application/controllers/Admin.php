c1ee2a6f6663746f941021cf2f05dae6eb267431
amiinrosyid98 <shodiqulad98@gmail.com>
2025-03-11 11:00:22 +0700

    second commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Admin extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,8 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('include/header');
-		$this->load->view('welcome_message');
-		$this->load->view('include/footer');
+		$this->load->view('admin/include/header');
+		$this->load->view('admin/index');
+		$this->load->view('admin/include/footer');
 	}
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,105 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Mamba Bootstrap Template</title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-
-  <!-- Favicons -->
-  <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
-  <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Main CSS File -->
-  <link href="<?= base_url() ?>assets/css/main.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Mamba
-  * Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body class="index-page">
-
-  <header id="header" class="header sticky-top">
-
-    <div class="topbar d-flex align-items-center">
-      <div class="container d-flex justify-content-center justify-content-md-between">
-        <div class="contact-info d-flex align-items-center">
-          <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
-        </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-          <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
-          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        </div>
-      </div>
-    </div><!-- End Top Bar -->
-
-    <div class="branding d-flex align-items-cente">
-
-      <div class="container position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-          <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src="<?= base_url() ?>assets/img/logo.png" alt=""> -->
-          <h1 class="sitename">Mamba</h1>
-        </a>
-
-        <nav id="navmenu" class="navmenu">
-          <ul>
-            <li><a href="#hero" class="active">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#team">Team</a></li>
-            <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="#">Dropdown 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Dropdown 2</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
-
-      </div>
-
-    </div>
-
-  </header>
-
-  <main class="main">
 
     <!-- Hero Section -->
     <section id="hero" class="hero section dark-background">
@@ -107,29 +6,31 @@
       <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
 
         <div class="carousel-item active">
-          <img src="<?= base_url() ?>assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
+          <img src="<?= base_url() ?>assets/img/hero-carousel/gema1.jpeg" alt="">
           <div class="carousel-container">
-            <h2>Welcome to Mamba<br></h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <a href="#featured-services" class="btn-get-started">Get Started</a>
+            <h2 class="text-center">Selamat Datang di <br>Gema Marina Nusantara</h2>
+            <p class="text-center">Pabrik industri yang bergerak dibidang alat-alat pelabuhan dan dermaga. Produk Rubber Fender yang sudah bersertifikat <strong>SNI 06-3568-2006</strong>, <strong>TKDN DAN ISO</strong></p>
+            <a href="#featured-services" class="btn-get-started">Mulai Sekarang</a>
           </div>
         </div><!-- End Carousel Item -->
 
         <div class="carousel-item">
           <img src="<?= base_url() ?>assets/img/hero-carousel/hero-carousel-2.jpg" alt="">
           <div class="carousel-container">
-            <h2>At vero eos et accusamus</h2>
-            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut.</p>
-            <a href="#featured-services" class="btn-get-started">Get Started</a>
+            <h2 class="text-center">Kenali Produk kami</h2>
+            <p class="text-center">Kami memproduksi diantaranya: Rubber Fender Type Arch/V, M, Element/PI, Cylinder, Square, D, Cell Fender, Rubber Hose, Elastomer, BearingPads, Expansion Joint, Bollard, Frontal Frame, dan Accesories Fender lainnya</p>
+            <a href="#featured-services" class="btn-get-started">Mulai Sekarang</a>
+
           </div>
         </div><!-- End Carousel Item -->
 
         <div class="carousel-item">
           <img src="<?= base_url() ?>assets/img/hero-carousel/hero-carousel-3.jpg" alt="">
           <div class="carousel-container">
-            <h2>Temporibus autem quibusdam</h2>
-            <p>Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt omnis iste natus error sit voluptatem accusantium.</p>
-            <a href="#featured-services" class="btn-get-started">Get Started</a>
+            <h2 class="text-center">Layanan kami</h2>
+            <p class="text-center"> Kami mengutamakan kepuasan pelanggan dalam memberikan produk dan jasa sesuai permintaan pelanggan dengan menggunakan prosedur perusahaan yang mengacu kepada sistem manajemen mutu, peraturan pemerintah serta undang-undang yang berlaku.</p>
+            <a href="#featured-services" class="btn-get-started">Mulai Sekarang</a>
+
           </div>
         </div><!-- End Carousel Item -->
 
@@ -152,33 +53,38 @@
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>About</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <h2>Tentang Kami</h2>
+        <p>Kami adalah penyedia solusi karet berkualitas tinggi untuk berbagai industri.</p>
       </div><!-- End Section Title -->
 
       <div class="container">
 
         <div class="row gy-4">
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <h3>Voluptatem dignissimos provident laboris nisi ut aliquip ex ea commodo</h3>
-            <img src="<?= base_url() ?>assets/img/about.jpg" class="img-fluid rounded-4 mb-4" alt="">
-            <p>Ut fugiat ut sunt quia veniam. Voluptate perferendis perspiciatis quod nisi et. Placeat debitis quia recusandae odit et consequatur voluptatem. Dignissimos pariatur consectetur fugiat voluptas ea.</p>
-            <p>Temporibus nihil enim deserunt sed ea. Provident sit expedita aut cupiditate nihil vitae quo officia vel. Blanditiis eligendi possimus et in cum. Quidem eos ut sint rem veniam qui. Ut ut repellendus nobis tempore doloribus debitis explicabo similique sit. Accusantium sed ut omnis beatae neque deleniti repellendus.</p>
+            <!-- <h3>Voluptatem dignissimos provident laboris nisi ut aliquip ex ea commodo</h3> -->
+            <img src="<?= base_url() ?>assets/img/hero-carousel/gema1.jpeg" class="img-fluid rounded-4 mb-4" alt="">
+            <p><b>CV GEMA MARINA NUSANTARA</b> adalah Pabrik industri yang bergerak dibidang alat-alat pelabuhan dan dermaga. Produk Rubber Fender yang sudah bersertifikat SNI 06-3568-2006 , TKDN DAN ISO. 
+
+            </p>
+            <p>Kami memproduksi diantaranya: Rubber Fender Type Arch/V, M, Element/PI, Cylinder, Square, D, Cell Fender, Rubber Hose, Elastomer, BearingPads, Expansion Joint, Bollard, Frontal Frame, dan Accesories Fender lainnya lainnya seperti LAMPU PJU, dimana seluruh produksi telah kami kirim ke berbagai proyek-proyek renovasi dan pembangunan dermaga baru di seluruh wilayah Indonesia.</p>
           </div>
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
             <div class="content ps-0 ps-lg-5">
+              <h3>Legalitas Perusahaan</h3>
               <p class="fst-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
+                Telah memiliki izin perusahaan seperti SIUP, HO, TDP, dan juga akta pendirian perusahaan yang sah
               </p>
+
               <ul>
-                <li><i class="bi bi-check-circle-fill"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-                <li><i class="bi bi-check-circle-fill"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-                <li><i class="bi bi-check-circle-fill"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>ISO 9001:2015 Rubber Manufacture for General Use and Infrastructure Use</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>ISO 14001:2015 Rubber Manufacture for General Use and Infrastructure Use</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>ISO 45001:2018 Rubber Manufacture for General Use and Infrastructure Use</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>Tingkat Komponen Dalam Negeri (TKDN) kami Mencapai 40%</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>SNI 06-3568-2006 Vulkanisat Karet Kompon Bantalan Dermaga</span></li>
               </ul>
+              
               <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                Berdasarkan pengalaman kami yang cukup panjang didunia Rubber Fender serta hubungan baik dengan klien-klien yang telah dibina selama ini, maka dengan tangan terbuka, kami membuka ruang bagi siapapun untuk bekerjasama dengan kami.
               </p>
 
               <div class="position-relative mt-4">
@@ -710,109 +616,4 @@
 
     </section><!-- /Contact Section -->
 
-  </main>
-
-  <footer id="footer" class="footer light-background">
-
-    <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Mamba</span>
-          </a>
-          <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Hic solutasetp</h4>
-          <ul>
-            <li><a href="#">Molestiae accusamus iure</a></li>
-            <li><a href="#">Excepturi dignissimos</a></li>
-            <li><a href="#">Suscipit distinctio</a></li>
-            <li><a href="#">Dilecta</a></li>
-            <li><a href="#">Sit quas consectetur</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Nobis illum</h4>
-          <ul>
-            <li><a href="#">Ipsam</a></li>
-            <li><a href="#">Laudantium dolorum</a></li>
-            <li><a href="#">Dinera</a></li>
-            <li><a href="#">Trodelas</a></li>
-            <li><a href="#">Flexo</a></li>
-          </ul>
-        </div>
-
-      </div>
-    </div>
-
-    <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Mamba</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-
-  </footer>
-
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Preloader -->
-  <div id="preloader"></div>
-
-  <!-- Vendor JS Files -->
-  <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/php-email-form/validate.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/aos/aos.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<?= base_url() ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-  <!-- Main JS File -->
-  <script src="<?= base_url() ?>assets/js/main.js"></script>
-
-</body>
-
-</html>
+  
